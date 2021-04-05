7476e24bc1f3478b9ce8aaa252cda5eb960093b6
Jason Stanley <js41074546@gmail.com>
2021-04-04 20:15:11 -0500

    Add files via upload

---
+++ fnnlsSelfmade.m
@@ -0,0 +1,88 @@
+
+t1 = cputime;
+
+%Precomputing AtA and Atb
+AtA = A'*A;
+Atb = A'*b;
+n = size(AtA,2);
+
+P = [];
+N = 1:n;
+
+x = zeros(n,1);
+w = (Atb) - (AtA)*x;
+wN = w(N);
+iters = 0;
+
+%Main Loop
+while(~(isempty(N) || all(wN<=0)))
+    %Moving active and passive set indices
+    wmax = max(wN);
+    t = find(w==wmax);
+    P = [P t];
+    N(N==t) = [];
+    
+    %Solving the LS problem
+    z = zeros(n,1);
+    An = AtA(P,P);
+    bn = Atb(P);
+    zp = An\bn;
+    z(N) = zeros(length(N),1);
+    z(P) = zp;
+    
+    %Inner loop handling the non-negativity constraint
+    while(min(zp)<=0)
+        xp = x(P);
+        ratio = zeros(length(xp),1);
+        for i=1:length(xp)
+            ratio(i) = xp(i)/(xp(i)-zp(i));
+        end
+        qind = zp<=0;
+        [alpha,pq] = min(ratio(qind));
+        q = P(pq);
+        x = x + alpha*(z-x);
+        temp = P(x(P)==0);
+        N = [N temp];
+        P(P==temp) = [];
+        An = AtA(P,P);
+        bn = Atb(P);
+        zp = An\bn;
+        z(N) = zeros(length(N),1);
+        z(P) = zp;      
+    end
+    x=z;
+    w = (Atb) - (AtA)*x;
+    wN = w(N);
+    iters = iters +1;
+end
+tsfnnls = cputime - t1;
+
+
+%{
+while(1)
+    w = A'*(b - A*x);   
+    if(isempty(N) || all(wN<=0))
+        break;
+    end
+    [wmax,t] = max(w);
+    P = [P t];
+    N(N==t) = [];
+    %REMEMBER TO CLEAR THE RIGHT VARIABLES
+    z = zeros(n,1);
+    Ap = A(:,P);
+    zp = lsqr(Ap,b);
+    z(N) = zeros(length(N),1);
+    z(P) = zp;
+    if(all(zp>0))
+        x = z; 
+        continue
+    end
+    xp = x(P);
+    [alpha,pq] = min(xp/(xp-zp));
+    q = P(pq);
+    x = x + alpha*(z-x);
+    temp = (xp==0);
+    N = [N P(temp)];
+    P(temp) = [];
+end
+%}
